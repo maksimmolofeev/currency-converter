1cbe49be86b2ff8372e3ad4fa65352b5b7d2162b
maksimmolofeev <109696557+maksimmolofeev@users.noreply.github.com>
2022-10-11 13:47:40 +0300

    finished

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
 const inputRubles = document.querySelector('.input_rubles');
 const btnNode = document.querySelector('.btn');
-const numberDollar = document.querySelector('.dollar_field');
-const numberLyre = document.querySelector('.lyre_field');
+const containerEl = document.querySelector('.working_field');
+const container =document.querySelector('.container')
+const select = document.querySelector('select')
 
-// Перевод в доллары
-function quantityDollars(number) {
-    let dollar = number / 60.25;
-    let answerDollar = Math.round(dollar * 100) / 100;
-    console.log = (answerDollar);
-    numberDollar.innerHTML = `<p>${answerDollar}</p>`;
-};
+function createElementFromHTML(htmlString) {
+    const div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+  
+    // Change this to div.childNodes to support multiple top-level nodes.
+    return div.firstChild;
+  }
 
-// Перевод в лиры
-function quantityLyre(number) {
-    let lyre = number / 3.24;
-    let answerLyre = Math.round(lyre * 100) / 100;
-    numberLyre.innerHTML = `<p>${answerLyre}</p>`;
+const currencyDictionary = {
+    "EUR": "Евро",
+    "USD": "Доллары",
+    "TRY": "Лиры",
+    "RUB": "Рубли"
 }
 
 // Получение кол-ва рублей
-btnNode.addEventListener('click', () => {
+btnNode.addEventListener('click', async () => {
+    console.log(select.value);
     let number = inputRubles.value;
-    console.log(number);
-    quantityDollars(number);
-    quantityLyre(number);
+
+    console.log(await (await fetch('https://www.cbr-xml-daily.ru/latest.js')).json());
+    console.log(await fetch('https://www.cbr-xml-daily.ru/latest.js'));
+
+
+    const { rates } = await (await fetch('https://www.cbr-xml-daily.ru/latest.js')).json();
+
+    container.classList.add('active');
+
+    containerEl.innerHTML = '';
+
+    Object.keys(rates).forEach((currencyCode)=> {
+        const title = currencyDictionary[currencyCode];
+
+        console.log(title);
+
+        if (title) {
+            containerEl.append(createElementFromHTML(`<div class="block_currency">
+                <h2>${title}</h2>
+                <div class="currency_field">
+                    <p>${Math.round(number * rates[currencyCode] * 100) / 100}</p>
+                </div>
+            </div>`));
+        }
+    });
 });
 
+// try..catch
